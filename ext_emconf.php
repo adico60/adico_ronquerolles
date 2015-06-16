8266045bce361e05cd8bf46b1b6d9b2fcc418191
YaNn <yann.berton@Yann-PC.domaine.local>
2015-06-16 12:03:31 +0200

    Initial Commit

---
+++ ext_emconf.php
@@ -0,0 +1,40 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "pradinestemplate".
+ *
+ * Auto generated 23-08-2014 13:00
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Adico theme Ronquerolles',
+	'description' => 'Theme Ronquerolles ',
+	'category' => 'fe',
+	'version' => '0.0.1',
+	'state' => 'beta',
+	'uploadfolder' => true,
+	'createDirs' => '',
+	'clearcacheonload' => true,
+	'author' => 'Yann Berton',
+	'author_email' => 'yann.berton@adico.fr',
+	'author_company' => 'ADICO',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'bootstrap_package' => '6.2.9-6.99.99',
+//			'news' => '6.2.9-6.99.99',
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
+);
+
